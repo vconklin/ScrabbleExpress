96aec72be5b63389ef6f6e6dd2aee39d5eead578
Valerie <val.conklin@gmail.com>
2016-06-13 16:05:28 -0700

    Set up basic routes and views for index chart and score

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Scrabble' });
 });
+
+/* GET chart page */
+router.get('/scrabble/chart', function(req, res, next) {
+  res.render('chart', { title: 'Chart'});
+});
+
+router.get('/scrabble/score', function(req, res, next) {
+  res.render('chart', { title: 'Score'});
+});
+
+
+
 
 module.exports = router;
